1542b432046daf101d69aedea5e97b2f3534c7b7
Nimrod Nagy <nimrod.nagy@lynxsolutions.eu>
2018-04-03 16:48:52 +0300

    Task #1 - fix:

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 
 sed -e "s~%%QUEUE_CONNECTION%%~$QUEUE_CONNECTION~" \
 		-e "s~%%QUEUE_NAME%%~$QUEUE_NAME~" \
-        /etc/supervisor/supervisord.conf
+        /etc/supervisord.conf
 
 exec /usr/bin/supervisord -n -c /etc/supervisord.conf
