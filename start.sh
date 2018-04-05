b5057639759f76dbd41a4dfa8251b02a3e5442fe
Nimrod Nagy <nimrod.nagy@lynxsolutions.eu>
2018-04-05 19:14:42 +0300

    Task #1 - AWS: refactor

--- start.sh
+++ start.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
+if [ -z "$QUEUE_TRIES" ]; then
+	QUEUE_TRIES="3"
+fi
+
+if [ -z "$QUEUE_TIMEOUT" ]; then
+	QUEUE_TIMEOUT="60"
+fi
+
 sed -i -e "s~%%QUEUE_CONNECTION%%~$QUEUE_CONNECTION~" \
 		-e "s~%%QUEUE_NAME%%~$QUEUE_NAME~" \
+        -e "s~%%QUEUE_TRIES%%~$QUEUE_TRIES~" \
+        -e "s~%%QUEUE_TIMEOUT%%~$QUEUE_TIMEOUT~" \
         /etc/supervisord.conf
 
 exec /usr/bin/supervisord -n -c /etc/supervisord.conf
