b99e24058240f72ba3dfbdc7eb846415e7d28b93
Nimrod Nagy <nimrod.nagy@lynxsolutions.eu>
2018-04-16 20:39:13 +0300

    Task #1 - AWS: refactor

--- start.sh
+++ start.sh
@@ -14,4 +14,10 @@ sed -i -e "s~%%QUEUE_CONNECTION%%~$QUEUE_CONNECTION~" \
         -e "s~%%QUEUE_TIMEOUT%%~$QUEUE_TIMEOUT~" \
         /etc/supervisord.conf
 
+echo "$OAUTH_PUBLIC" >  /var/www/html/storage/oauth-public.key
+echo "$OAUTH_PRIVATE" >  /var/www/html/storage/oauth-private.key
+
+chmod 600 /var/www/html/storage/oauth-public.key
+chmod 600 /var/www/html/storage/oauth-private.key
+
 exec /usr/bin/supervisord -n -c /etc/supervisord.conf
